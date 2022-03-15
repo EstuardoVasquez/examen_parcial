7cbb04ec8b394f3c8501d95128c188a00201e0d2
Estuardo Vasquez <vasquezalvarado@hotmail.es>
2022-03-15 09:42:21 -0600

    Creación de programa en Phyton
    Aquí introduje un programa que calcula el área de un lugar.

---
+++ Programa.py
@@ -0,0 +1,6 @@
+largo = float(input("Ingrese el largo: "))
+ancho = float(input("Ingrese el ancho: "))
+profundidad = float(input("Ingrese la profundidad promedio: "))
+perimetro = largo * ancho * profundidad
+print("El perímetro es: ", perimetro)
+
